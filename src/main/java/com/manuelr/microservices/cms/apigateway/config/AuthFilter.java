fd1776c87c0f8b5b7d6d190a27fb83fc1ec8a20e
manuelrojas19 <manuelarr99@gmail.com>
2022-02-03 12:25:26 -0600

    Configuration profiles added.

--- src/main/java/com/manuelr/microservices/cms/apigateway/config/AuthFilter.java
+++ src/main/java/com/manuelr/microservices/cms/apigateway/config/AuthFilter.java
@@ -17,11 +17,14 @@ import java.util.Collection;
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
-    private static final String VALIDATE_TOKEN_URI = "http://auth-server/api/v1/auth/validate_token";
-    private static final String AUTH_HEADER_NAME = "X-Auth-Token";
+    @Value("${cms.authentication.authentication-header-name}")
+    private String authHeaderName;
 
-    @Value("${authentication-dev.auth.accessTokenCookieName}")
+    @Value("${cms.authentication.access-token-cookie-name}")
     private String accessTokenCookieName;
+
+    @Value("${cms.authentication.validate-token-uri}")
+    private String validateTokenUri;
 
     private final WebClient.Builder webClientBuilder;
 
@@ -37,13 +40,13 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
             log.info("Api Gateway Encrypted Token ---> {}", encryptedJwtToken);
             return webClientBuilder.build()
                     .post()
-                    .uri(VALIDATE_TOKEN_URI)
+                    .uri(validateTokenUri)
                     .cookie(accessTokenCookieName, encryptedJwtToken)
                     .retrieve().bodyToMono(UserDto.class)
                     .map(userDto -> {
                         exchange.getRequest()
                                 .mutate()
-                                .header(AUTH_HEADER_NAME, encryptedJwtToken);
+                                .header(authHeaderName, encryptedJwtToken);
                         return exchange;
                     }).flatMap(chain::filter);
         };
